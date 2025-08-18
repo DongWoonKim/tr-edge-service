712ffe87f1fcc73d597499350836866293b345c1
DongWoonKim <01030985843z@naver.com>
2025-08-18 15:17:44 +0900

    chore/1-project-setup (Closes #1)

---
+++ src/main/java/com/trevari/spring/tredgeservice/TrEdgeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trevari.spring.tredgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrEdgeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrEdgeServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trevari/spring/tredgeservice/TrEdgeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trevari.spring.tredgeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrEdgeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
